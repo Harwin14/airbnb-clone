fc28c83165ad66986e1c65e4b337a8c965b14cf0
Harwin14 <agungharwinn@gmail.com>
2023-07-20 00:57:31 +0700

    upload photo

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Perks from "../components/Perks";
+import { PhotosUploader } from "../components/PhotosUploader";
 
 export default function PlacesPage() {
     const { action } = useParams();
+    const [title, setTitle] = useState("");
+    const [address, setAddress] = useState("");
+    const [addedPhotos, setAddedPhotos]= useState([])
+    const [description, setDescription] = useState("");
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState("");
+    const [checkIn, setCheckIn] = useState("");
+    const [checkOut, setCheckOut] = useState("");
+    const [maxGuests, setMaxGuests] = useState(1);
+
+    const inputHeader = (title) => {
+        return <h2 className="text-xl mt-4">{title}</h2>;
+    };
+    const inputDescription = (text) => {
+        return <p className="text-gray-500 text-sm">{text}</p>;
+    };
+    const preInput = (header, description) => {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        );
+    };
+
     return (
         <div>
             {action !== "new" && (
@@ -26,29 +55,93 @@ export default function PlacesPage() {
                     </Link>
                 </div>
             )}
-            {action === 'new' && (
+            {action === "new" && (
                 <div>
+                    <form>
+                        {preInput(
+                            "Title",
+                            "title for your place, should be short and catchy as in advertisement"
+                        )}
+                        <input
+                            type="text"
+                            placeholder="title, for example: My Lovely Apt"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
 
-                <form>
-                    <h2 className="text-xl mt-4">Title</h2>
-                    <p className="text-gray-500 text-sm">title for your place, should be short and catchy as in advertisement</p>
-                    <input type="text" placeholder="title, for example: My Lovely Apt" />
-                    <h2 className="text-xl mt-4">Address</h2>
-                    <p className="text-gray-500 text-sm">Address is this place</p>
-                    <input type="text" placeholder="address" />
-                    <h2 className="text-xl mt-4">Photos</h2>
-                    <p className="text-gray-500 text-sm">more = better</p>
-                    <div className="flex gap-2">
-                        <input type="text" placeholder={'Add using a link ... jpg'} />
-                        <button className="bg-gray-200 px-4">Add photo</button>
-                    </div>
-                    <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                    <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">+</button>
-                    </div>
-                </form>
+                        {preInput("Address", "Address is this place")}
+                        <input
+                            type="text"
+                            placeholder="address"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+
+                        {preInput("Photos", "more = better")}
+                        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
+
+                        {preInput("Description", "description of the place")}
+                        <textarea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+
+                        {preInput(
+                            "Perks",
+                            "select all the perks of your place"
+                        )}
+                        <div className="mt-2 grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                            <Perks selected={perks} onChange={setPerks} />
+                        </div>
+
+                        {preInput("Extra Info", "house rules, etc")}
+                        <textarea
+                            value={extraInfo}
+                            onChange={(e) => setExtraInfo(e.target.value)}
+                        />
+
+                        {preInput(
+                            " Check in&out times, max guests",
+                            "add check in and out times, remember to have sometime window for cleaning the room between guests"
+                        )}
+                        <div className="grid gap-2 sm:grid-cols-3">
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check in time</h3>
+                                <input
+                                    type="text"
+                                    placeholder="14"
+                                    value={checkIn}
+                                    onChange={(e) => setCheckIn(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check out time</h3>
+                                <input
+                                    type="text"
+                                    placeholder="11"
+                                    value={checkOut}
+                                    onChange={(e) =>
+                                        setCheckOut(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">
+                                    Max number of guests
+                                </h3>
+                                <input
+                                    type="number"
+                                    value={maxGuests}
+                                    onChange={(e) =>
+                                        setMaxGuests(e.target.value)
+                                    }
+                                />
+                            </div>
+                        </div>
+                        <button className="primary my-4">Save</button>
+                    </form>
                 </div>
             )}
         </div>
     );
 }
-      
