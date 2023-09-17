07c2e1efb8744911d73818e11ba7938c19b5c348
Harwin14 <agungharwinn@gmail.com>
2023-09-17 19:51:55 +0700

    r

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const userDoc = await User.create({
+    const userDoc = await User.create({ 
       name, 
       email,
       password: bcrypt.hashSync(password, bcryptSalt),
